ef290f896a932846b67634a2b44303f7bece8cd3
John Rose <johnedmundrose@gmail.com>
2022-08-09 17:27:42 -0400

    Fancy root route, fancy auto page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,6 +3,12 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", /home/i
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
