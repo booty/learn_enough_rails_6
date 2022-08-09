401d412efbc581ba52e30fc767d8cf753f056327
John Rose <johnedmundrose@gmail.com>
2022-08-09 01:06:56 -0400

    Fancier tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,5 +16,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "h1", /About/
   end
 end
