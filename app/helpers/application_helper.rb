db36e306d53a8dc6d850a91acb62910f3f7e81a8
John Rose <johnedmundrose@gmail.com>
2022-08-09 19:01:32 -0400

    Totally sick helper. CRUSHING IT

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  EMOJI = %w[🧐 ❤️ 🔥 💦 🍆 💪 🐶 🦄].freeze
+
+  def page_title(some_shit, context)
+    "#{some_shit.presence || context.action_name.titleize} | #{EMOJI.sample} Sample App!!!!"
+  end
 end
